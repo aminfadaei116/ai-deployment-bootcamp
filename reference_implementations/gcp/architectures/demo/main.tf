68f6ce49efe230f993e2eeb18fc61143ecac1ea7
aminfadaei116 <aminfadaeinejad.edu@gmail.com>
2024-09-10 21:37:15 -0400

    Update main.tf

--- reference_implementations/gcp/architectures/demo/main.tf
+++ reference_implementations/gcp/architectures/demo/main.tf
@@ -2,3 +2,22 @@ provider "google" {
   project = "ai-deployment-bootcamp-430513"
   region  = "us-central1"
 }
+
+resource "google_service_account" "sa" {
+  account_id = "demo-service-account"
+  display_name = "The demo service account"
+}
+
+#resource "google_project_iam_member" "storage_object_viewer" {
+#  project = "ai-deployment-bootcamp-430513"
+#  role    = "roles/aiplatform.user"
+#  member  = "serviceAccount:${google_service_account.sa.email}"
+#}
+
+output "service_account_id" {
+  value = google_service_account.sa.id
+}
+
+output "service_account_email" {
+  value = google_service_account.sa.email
+}
